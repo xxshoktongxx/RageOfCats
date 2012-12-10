0a7ada18f78702a063c899c3a7759ee8a6f134a6
xxshoktongxx <martin.magalong@ripple-wave.com>
2012-12-10 23:44:20 +0800

    Initial Commit

---
+++ RageOfCats/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  RageOfCats
+//
+//  Created by martin magalong on 12/10/12.
+//  Copyright (c) 2012 ripplewave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ RageOfCats/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  RageOfCats
+//
+//  Created by martin magalong on 12/10/12.
+//  Copyright (c) 2012 ripplewave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
